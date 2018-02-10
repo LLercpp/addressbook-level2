502cdc0f7d57f913921ef3477a94e54694940abc
LLercpp <yuanquan1995@gmail.com>
2018-02-10 22:26:26 +0800

    Revert "Update test cases"
    This reverts commit 5d48656c5cccf88cccc21ff3ca4d4da235948497.

--- test/java/seedu/addressbook/data/AddressBookTest.java
+++ test/java/seedu/addressbook/data/AddressBookTest.java
@@ -47,25 +47,25 @@ public class AddressBookTest {
         aliceBetsy     = new Person(new Name("Alice Betsy"),
                                     new Phone("91235468", false),
                                     new Email("alice@nushackers.org", false),
-                                    new Address("11, 8 Computing Drive, #1-1, Singapore 111111", false),
+                                    new Address("8 Computing Drive, Singapore", false),
                                     new UniqueTagList(tagMathematician));
 
         bobChaplin     = new Person(new Name("Bob Chaplin"),
                                     new Phone("94321500", false),
                                     new Email("bob@nusgreyhats.org", false),
-                                    new Address("22, 9 Computing Drive, #2-2. 2222222", false),
+                                    new Address("9 Computing Drive", false),
                                     new UniqueTagList(tagMathematician));
 
         charlieDouglas = new Person(new Name("Charlie Douglas"),
                                     new Phone("98751365", false),
                                     new Email("charlie@nusgdg.org", false),
-                                    new Address("33, 10 Science Drive, #3-3, 333333", false),
+                                    new Address("10 Science Drive", false),
                                     new UniqueTagList(tagScientist));
 
         davidElliot    = new Person(new Name("David Elliot"),
                                     new Phone("84512575", false),
                                     new Email("douglas@nuscomputing.com", false),
-                                    new Address("44 , 11 Arts Link, #4-44, 4444444", false),
+                                    new Address("11 Arts Link", false),
                                     new UniqueTagList(tagEconomist, tagPrizeWinner));
 
         emptyAddressBook = new AddressBook();
