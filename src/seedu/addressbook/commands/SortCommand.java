ca776a56d2227e2a428ff1eeb0661a22211bdb13
LLercpp <yuanquan1995@gmail.com>
2018-02-13 23:25:13 +0800

    Complete sort command

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -1,9 +1,44 @@
 package seedu.addressbook.commands;
 
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+
 public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Sort the record according to name.\n"
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Sort the record in lexicographical order by  name.\n"
             + "Example: " + COMMAND_WORD;
-    public static final String SORT_COMPLETED_ACKNOWEDGEMENT = "Sorting Address Book as requested ...";
+
+
+    public SortCommand(){
+
+    }
+
+    @Override
+    public CommandResult execute(){
+        List<? extends ReadOnlyPerson> sortedPersons = new ArrayList<>(
+                addressBook.getAllPersons().immutableListView());
+        sortedPersons.sort(new Comparator<ReadOnlyPerson>() {
+            @Override
+            public int compare(ReadOnlyPerson o1, ReadOnlyPerson o2) {
+                if(o1 == null){
+                    if(o2 == null){
+                        return 0;
+                    } else {
+                        return -1;
+                    }
+                } else if (o2 == null ) {
+                    return 1;
+                } else {
+                    return o1.getName().fullName.compareTo(o2.getName().fullName);
+                }
+            }
+        });
+        return new CommandResult( getMessageForPersonListShownSummary(sortedPersons),
+                sortedPersons);
+    }
 }
